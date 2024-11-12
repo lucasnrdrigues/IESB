51010a4362bae09f21421d0a7700baa0bc5d26c2
lucasnrdrigues <lucasnrdrigues@gmail.com>
2024-11-12 20:10:29 -0300

    Subindo pasta 2 prova

---
+++ 4SEM/FRONT/njsoft/src/app/page.js
@@ -0,0 +1,15 @@
+import styles from "./page.module.css";
+import Formulario from '../app/components/Formulario'
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <Formulario />
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
